ec0ff6c72f444980b6f8320beeaf5ce25a9b9d97
Tamilselvan <tamil.selvan@arus.sg>
2026-09-09 23:49:01 +0530

    fix: main-jsx-file

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,11 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 
+const routerBasename = import.meta.env.PROD ? '/Final-Portfolio' : '/'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={routerBasename}>
       <App />
     </BrowserRouter>
   </StrictMode>,
--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: process.env.NODE_ENV === 'production' ? '/Final-Portfolio/' : '/',
   plugins: [react(), tailwindcss()],
 })
